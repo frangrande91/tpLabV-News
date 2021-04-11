79509c015c37687ed063e1c0f857a76b15c54a06
Francisco Grande <franciscogrande91@gmail.com>
2021-04-11 20:54:33 -0300

    create class New

--- src/main/java/com/utn/tpnews/model/New.java
+++ src/main/java/com/utn/tpnews/model/New.java
@@ -1,4 +1,26 @@
 package com.utn.tpnews.model;
 
-public class New {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class New  {
+
+    @Id
+    @GeneratedValue
+    protected Integer id;
+
+    @NotNull
+    protected String title;
+
+    protected Integer size;
+
+
 }
